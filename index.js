b07f8551a21043dceb4597670005684d22472e1c
Antimio <antimiocasimiro@gmail.com>
2024-02-28 08:27:14 +0000

    Added an array of objects (to be passed into an inquirer promisse) with questions pertaining to the Manager object

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const questions_manager = [{
+        name: "manager_name",
+        type: "input",
+        message: "What's the team manager's name?:"
+    },
+    {
+        name: "manager_ID",
+        type: "input",
+        message: "What's the team manager's employee ID?:"
+    },
+    {
+        name: "manager_email",
+        type: "input",
+        message: "What's the team manager's email address?:"
+    },
+    {
+        name: "manager_officeNumber",
+        type: "input",
+        message: "What's the team manager's office number?:"
+    }
+]
